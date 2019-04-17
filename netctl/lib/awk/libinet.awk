serhepopovych/libusrxml

commit ae16d70e08e8828963a89bd0d34b3e72842896b0

libinet: Add helper routines to handle mac, ipv4 and ipv6 addresses
Core routines used to validate and normalize address:
-----------------------------------------------------

  do_mac()  - accept MAC address in one of the following formats:
                - dotted-word (cisco) (e.g. 1122.3344.5566)
                - dash separated (e.g. 11-22-33-44-55-66)
                - colon separated (e.g. 11:22:33:44:55:66)
              and convert it to colon separated format validaing input.

  do_ipv4() - accepts IPv4 address in dotted-quad notation, validates
              it and converts to normalized form (i.e. 192.000.02.1 ->
              192.0.2.1).

  do_ipv6() - accepts IPv6 in short, with IPv4 as last two words
              (i.e. ::ffff:192.0.2.1) or full form and converts it to
              minimal full form validating input
              (i.e. ::ffff:1.01.001.1 -> 0:0:0:0:0:ffff:101:101).

IPv4/IPv6 accept single parameter: address represented by string. They
should be considered as low-level and already used in other library
routines implemntation. MAC accepts two parameters: address represented
by string and character/string used as field separator (e.g. ".", "-"
and ":").

On failure empty ("") string is returned.

Routines based on the above:
----------------------------

  mac2colon(str) - use ":" as address field separator
  mac2dash(str)  - use "-" as address field separator
  mac2cisco(str) - use "." dotted-word address format (cisco)
  mac2raw(str)   - use "" as address field separator

On failure empty ("") string is returned.

  is_valid_mac(str)  - return 1 if valid MAC supplied in @str param
  is_valid_ipv4(str) - return 1 if valid IPv4 supplied in @str param
  is_valid_ipv6(str) - return 1 if valid IPv6 supplied in @str param

On failure (e.g. @str does not represent valid address) 0 is returned.

Helpers to convert IPv4 address to a 32-bit integer value:
----------------------------------------------------------

netctl/lib/awk/libinet.awk
@@ -1,0 +1,440 @@
+#!/usr/bin/gawk -f
+
+function do_mac(str, sep,    nfields, v, s, o, t, ret)
+{
+	if (sep != ":" && sep != "-" && sep != "." && sep != "")
+		return "";
+
+	nfields = split(str, s, "[[:xdigit:]]{2}", o);
+	if (nfields != 7)
+		return "";
+
+	# begin and end are ""
+	if (s[1] != "" || s[7] != "")
+		return "";
+
+	v = s[3];
+	if (v == ".") {
+		# 1122.3344.5566
+		if (s[5] != ".")
+			return "";
+		t = "";
+		# mark entry for [s]kip
+		s[5] = "s";
+	} else if (v == ":" || v == "-" || v == "") {
+		# 11:22:33:44:55:66, 11-22-33-44-55-66 and 112233445566
+		t = v;
+	} else {
+		return "";
+	}
+
+	# mark entries for [s]kip
+	s[1] = s[7] = s[3] = "s";
+
+	for (v = 1; v <= nfields; v++) {
+		if (s[v] == "s")
+			continue;
+		if (s[v] != t)
+			return "";
+	}
+
+	ret = "";
+
+	for (v = 1; v < nfields; v++) {
+		if (v >= nfields - 1)
+			t = "";
+		else if (sep == ".")
+			t = (v % 2 == 0) ? sep : "";
+		else
+			t = sep;
+		ret = ret o[v] t;
+	}
+
+	return ret;
+}
+
+function mac2colon(str)
+{
+	return do_mac(str, ":");
+}
+
+function mac2dash(str)
+{
+	return do_mac(str, "-");
+}
+
+function mac2cisco(str)
+{
+	return do_mac(str, ".");
+}
+
+function mac2raw(str)
+{
+	return do_mac(str, "");
+}
+
+function is_valid_mac(str)
+{
+	return do_mac(str, "") != "";
+}
+
+function do_ipv4(str,    nfields, v, vals)
+{
+	nfields = split(str, vals, ".");
+	# "1.1.1.1" gives four fields
+	if (nfields != 4)
+		return "";
+	while (nfields) {
+		v = vals[nfields];
+		# there might be '-' or '+'
+		if (v !~ "^[[:digit:]]+$")
+			return 0;
+		if (v < 0 || v > 255)
+			return 0;
+		nfields--;
+	}
+	return sprintf("%d.%d.%d.%d", vals[1], vals[2], vals[3], vals[4]);
+}
+
+function is_valid_ipv4(str)
+{
+	return do_ipv4(str) != "";
+}
+
+function __ipv4_to_int(str,    nfields, v, vals)
+{
+	nfields = split(str, vals, ".");
+	if (nfields != 4)
+		return -1;
+
+	vals[1] = lshift(vals[1], 24);
+	vals[2] = lshift(vals[2], 16);
+	vals[3] = lshift(vals[3],  8);
+	vals[4] = lshift(vals[4],  0);
+
+	vals[1] = or(vals[1], vals[4]);
+	vals[2] = or(vals[2], vals[3]);
+
+	return or(vals[1], vals[2]);
+}
+
+function ipv4_to_int(str)
+{
+	str = do_ipv4(str);
+	if (str == "")
+		return -1;
+	return __ipv4_to_int(str);
+}
+
+function do_ipv6(str,    nfields, v, vals, mfields, n, short, nshort, ishort)
+{
+	n = nfields = split(str, vals, ":");
+
+	# "::" gives three fields
+	if (nfields < 3)
+		return "";
+
+	# empty, hex or dotted-quad
+	v = vals[n];
+	if (v !~ "^[[:xdigit:]]{0,4}$") {
+		v = do_ipv4(v)
+		if (v == "")
+			return v;
+		short = __ipv4_to_int(v);
+		# 1:2:3:4:5:6:1.1.1.1 -> 1:2:3:4:5:6:0101:0101
+		vals[n] = sprintf("%x", rshift(short, 16));
+		n = ++nfields;
+		vals[n] = sprintf("%x", and(short, 0xffff));
+	}
+
+	# 1:2:3:4:5:6:7::
+	short = v == "";
+
+	# 1:2:3:4:5:6:7:8 or 1:2:3:4:5:6:7::
+	mfields = 8 + short;
+
+	# ::1:2:3:4:5:6:7
+	if (vals[1] == "")
+		mfields++;
+
+	# no more than max fields
+	if (nfields > mfields)
+		return "";
+
+	nshort = short;
+	ishort = 0;
+
+	# :1, :1:2, ...
+	vals[0] = "1";
+
+	# :1:2
+	while (n > 0) {
+		v = vals[--n];
+		if (v == "") {
+			if (short > 1) {
+				# ::, ::1, ::1:2, ...
+				if (n <= 1)
+					break;
+				return "";
+			}
+			if (n > 1) {
+				nshort += n;
+				# 1:::2
+				short++;
+			}
+			short++;
+		} else {
+			# :1, :1:2, ..., 1:, 1:2:, ...
+			if (short == 1)
+				return "";
+			if (v !~ "^[[:xdigit:]]{1,4}$")
+				return "";
+			if (short && !ishort) {
+				nshort -= n;
+				ishort = n;
+			}
+		}
+	}
+
+	# no short: max fields
+	if (!short && nfields != mfields)
+		return "";
+
+	mfields = 8;
+	v = "";
+
+	for (n = 1; n <= ishort; n++) {
+		short = vals[n];
+		short = strtonum("0x" short);
+		v = v sprintf("%x:", short);
+	}
+
+	nshort = mfields - (nfields - nshort);
+	while (nshort) {
+		v = v "0:";
+		nshort--;
+	}
+
+	for (; n <= nfields; n++) {
+		short = vals[n];
+		if (short == "")
+			continue;
+		short = strtonum("0x" short);
+		v = v sprintf("%x:", short);
+	}
+
+	gsub(":$", "", v);
+
+	return v;
+}
+
+function is_valid_ipv6(str)
+{
+	return do_ipv6(str) != "";
+}
+
+function ipa_normalize(str,    addr)
+{
+	addr = do_ipv4(str);
+	if (addr != "")
+		return addr;
+	addr = do_ipv6(str);
+	if (addr != "")
+		return addr;
+	return "";
+}
+
+function ipa_compare(str1, str2,    v1, v2)
+{
+	v1 = ipa_normalize(str1);
+	if (v1 == "")
+		return -1;
+	v2 = ipa_normalize(str2);
+	if (v2 == "")
+		return -1;
+	return v1 == v2;
+}
+
+function ipa_equal(str1, str2)
+{
+	return ipa_compare(str1, str2) == 1;
+}
+
+function ipa_not_equal(str1, str2)
+{
+	return ipa_compare(str1, str2) == 0;
+}
+
+function ipp_length(str,    nfields, vals, plen)
+{
+	nfields = split(str, vals, "/");
+	if (nfields != 2)
+		return -1;
+
+	plen = vals[2];
+	if (plen !~ "^[[:digit:]]{1,3}$")
+		return -1;
+
+	if (plen > 128)
+		return -1;
+
+	return plen;
+}
+
+function ipp_normalize(str,    nfields, vals, addr, plen)
+{
+	nfields = split(str, vals, "/");
+	if (nfields != 2)
+		return "";
+
+	plen = vals[2];
+	if (plen !~ "^[[:digit:]]{1,3}$")
+		return "";
+
+	addr = do_ipv4(vals[1]);
+	if (addr != "") {
+		if (plen > 32)
+			return "";
+	} else {
+		addr = do_ipv6(vals[1]);
+		if (addr == "")
+			return "";
+		if (plen > 128)
+			return "";
+	}
+
+	return addr "/" plen;
+}
+
+function ipp_network(str,    nfields, o, b, m, vals, sep, addr, plen)
+{
+	nfields = split(str, vals, "/");
+	if (nfields != 2)
+		return "";
+
+	plen = vals[2];
+	if (plen !~ "^[[:digit:]]{1,3}$")
+		return "";
+
+	addr = do_ipv4(vals[1]);
+	if (addr != "") {
+		if (plen > 32)
+			return "";
+		sep = ".";
+		split(addr, vals, sep);
+		o = int(plen / 8) + 1;
+		b = plen % 8;
+		if (b != 0) {
+			m = compl(rshift(0xff, b));
+			vals[o] = and(vals[o], m);
+			o++;
+		}
+	} else {
+		addr = do_ipv6(vals[1]);
+		if (addr == "")
+			return "";
+		if (plen > 128)
+			return "";
+		sep = ":";
+		split(addr, vals, sep);
+		o = int(plen / 16) + 1;
+		b = plen % 16;
+		if (b != 0) {
+			m = compl(rshift(0xffff, b));
+			vals[o] = sprintf("%x", and(strtonum("0x" vals[o]), m));
+			o++;
+		}
+	}
+
+	while (vals[o] != "")
+		vals[o++] = "0";
+
+	str = "";
+	for (o = 1; vals[o] != ""; o++)
+		str = str vals[o] sep;
+	gsub(sep "$", "", str);
+	str = str "/" plen;
+
+	return str;
+}
+
+function is_ipp_network(str)
+{
+	str = ipp_normalize(str);
+	if (str == "")
+		return 0;
+	return ipp_network(str) == str;
+}
+
+function is_ipp_host(str)
+{
+	str = ipp_normalize(str);
+	if (str == "")
+		return 0;
+	return ipp_network(str) != str;
+}
+
+function ipa_do_match(net, address,    nfields, o, b, m, v, valsN, valsA, sep, addr, plen)
+{
+	nfields = split(net, valsN, "/");
+	if (nfields != 2)
+		return -1;
+
+	plen = valsN[2];
+	if (plen !~ "^[[:digit:]]{1,3}$")
+		return -1;
+
+	address = ipa_normalize(address);
+	if (address == "")
+		return -1;
+	split(address, valsA, "[.:]", sep);
+
+	addr = do_ipv4(valsN[1]);
+	if (addr != "") {
+		if (plen > 32)
+			return -1;
+		if (sep[1] != ".")
+			return -1;
+		split(addr, valsN, ".");
+		o = int(plen / 8) + 1;
+		b = plen % 8;
+		if (b != 0)
+			m = compl(rshift(0xff, b));
+	} else {
+		addr = do_ipv6(valsN[1]);
+		if (addr == "")
+			return -1;
+		if (plen > 128)
+			return -1;
+		if (sep[1] != ":")
+			return -1;
+		split(addr, valsN, ":");
+		o = int(plen / 16) + 1;
+		b = plen % 16;
+		if (b != 0)
+			m = compl(rshift(0xffff, b));
+	}
+
+	if (b != 0) {
+		v = xor(valsN[o], valsA[o]);
+		if (and(v, m))
+			return 0;
+	}
+
+	while (--o > 0) {
+		if (valsN[o] != valsA[o])
+			return 0;
+	}
+
+	return 1;
+}
+
+function ipa_match(net, address)
+{
+	return ipa_do_match(net, address) == 1;
+}
+
+function ipa_not_match(net, address)
+{
+	return ipa_do_match(net, address) == 0;
+}

commit c28101f14f4e9739445b02ba092f140bc4e7f679

libinet: Return empty string on IPv4 parse errors in all cases
Returning 0 confuses caller as it returns neither valid IP address nor
valid error value (empty string) since zero IP address is fully legal.

Fixes: ae16d70e08e8 ("libinet: Add helper routines to handle mac, ipv4 and ipv6 addresses")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libinet.awk
@@ -88,9 +88,9 @@ function do_ipv4(str,    nfields, v, vals)
 		v = vals[nfields];
 		# there might be '-' or '+'
 		if (v !~ "^[[:digit:]]+$")
-			return 0;
+			return "";
 		if (v < 0 || v > 255)
-			return 0;
+			return "";
 		nfields--;
 	}
 	return sprintf("%d.%d.%d.%d", vals[1], vals[2], vals[3], vals[4]);