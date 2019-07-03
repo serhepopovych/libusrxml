serhepopovych/libusrxml

commit 57444276950b5c3932c9f48679fb1062634a4d06

libusrxml: Skip lines belonging to failed user entry until new entry
We left parser scope pointing to one we failed to parse current user
entry and restore previous, or delete if adding new, user data.

If code calling run_usrxml_parser() is designed to skip failed entries
(not currently applicable to load_usrxml_file() helper), we continue on
failed entry returning failure again and trying to restore/delete
user data.

Instead introduce new parser state: error that intended to skip all
lines until next valid user entry begins. This allows to implement code
that skips failed entries.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -309,12 +309,14 @@ function declare_usrxml_consts()
 	USRXML_E_HANDLE_FULL    = -202;
 	USRXML_E_API_ORDER      = -203;
 	USRXML_E_GETLINE        = -204;
+	USRXML_E_EATLINE        = -205;
 	# entry
 	USRXML_E_NOENT	= -301;
 
 	## Constants (internal, arrays get cleaned )
 
 	# Tag scope
+	USRXML__scope_error	= -1;
 	USRXML__scope_none	= 0;
 	USRXML__scope_user	= 1;
 	USRXML__scope_pipe	= 2;
@@ -322,6 +324,7 @@ function declare_usrxml_consts()
 	USRXML__scope_net	= 4;
 	USRXML__scope_net6	= 5;
 
+	USRXML__scope2name[USRXML__scope_error]	= "error";
 	USRXML__scope2name[USRXML__scope_none]	= "none";
 	USRXML__scope2name[USRXML__scope_user]	= "user";
 	USRXML__scope2name[USRXML__scope_pipe]	= "pipe";
@@ -1162,6 +1165,20 @@ function fini_usrxml_parser(h,    n, u)
 # Parse and validate XML document.
 #
 
+function usrxml__scope_error(h, name, val)
+{
+	# Skip all lines until new user entry on error
+
+	if (name == "user") {
+		USRXML__instance[h,"scope"] = USRXML__scope_none;
+
+		# Signal caller to lookup with new scope
+		return 1;
+	}
+
+	return USRXML_E_EATLINE;
+}
+
 function usrxml__scope_none(h, name, val,    n, i)
 {
 	if (name == "user") {
@@ -1555,6 +1572,13 @@ function run_usrxml_parser(h, line, cb, data,    a, nfields, fn, val)
 				val = @cb(h, val, data);
 			else
 				val++;
+			break;
+		}
+		# Parse error in the middle of operation: skip lines until valid
+		if (val < 0) {
+			if (val == USRXML_E_EATLINE)
+				return USRXML_E_NONE;
+			USRXML__instance[h,"scope"] = USRXML__scope_error;
 			break;
 		}
 	} while (val > 0);

commit 3bad3f7a2b72e8177b02a677e92ab0460542c73f

libusrxml: Improve run_usrxml_parser()
We can legally return USRXML_E_NONE now from usrxml__scope_error()
handler as this will skip all activities in run_usrxml_parser()
anyway.

Set scope to USRXML__scope_error only when scope failed to parse is
greather than USRXML__scope_none. Add check for user close tag to
error scope parse function to skip only lines until either new
user section opened or currently failed closed. This is to only skip
lines belonging to failed entry.

Always call scope specific parser function even if @line does not
match(). Pass empty @name to force that function to fail as empty
names isn't legal (see regular expression passed to match()).

Fixes: 57444276950b ("libusrxml: Skip lines belonging to failed user entry until new entry")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -605,7 +605,6 @@ function declare_usrxml_consts()
 	USRXML_E_HANDLE_FULL	= -(USRXML_E_BASE + 102);
 	USRXML_E_API_ORDER	= -(USRXML_E_BASE + 103);
 	USRXML_E_GETLINE	= -(USRXML_E_BASE + 104);
-	USRXML_E_EATLINE	= -(USRXML_E_BASE + 105);
 
 	# Generic
 	USRXML_E_NOENT		= -(USRXML_E_BASE + 201);
@@ -1617,9 +1616,11 @@ function usrxml__scope_error(h, sign, name, val)
 
 		# Signal caller to lookup with new scope
 		return 1;
+	} else if (name == "/user") {
+		USRXML__instance[h,"scope"] = USRXML__scope_none;
 	}
 
-	return USRXML_E_EATLINE;
+	return USRXML_E_NONE;
 }
 
 function usrxml__scope_none(h, sign, name, val,    n, i)
@@ -2029,8 +2030,11 @@ function run_usrxml_parser(h, line, cb, data,    a, n, fn, sign, name, val, ret,
 	RSTART = s_rs;
 	RLENGTH = s_rl;
 
+	# On line mismatch make sure name (a[2]) is empty to
+	# trigger syntax error at any scope as it cannot be
+	# empty (see match() regular expression above).
 	if (!n)
-		return usrxml_syntax_err(h);
+		a[1] = a[2] = a[3] = "";
 
 	sign = a[1];
 	name = a[2];
@@ -2064,9 +2068,8 @@ function run_usrxml_parser(h, line, cb, data,    a, n, fn, sign, name, val, ret,
 		}
 		# Parse error in the middle of operation: skip lines until valid
 		if (ret < 0) {
-			if (ret == USRXML_E_EATLINE)
-				return USRXML_E_NONE;
-			USRXML__instance[h,"scope"] = USRXML__scope_error;
+			if (n > USRXML__scope_none)
+				USRXML__instance[h,"scope"] = USRXML__scope_error;
 			break;
 		}
 	} while (ret > 0);