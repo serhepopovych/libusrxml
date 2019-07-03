serhepopovych/libusrxml

commit 57444276950b5c3932c9f48679fb1062634a4d06

libusrxml: Skip lines belonging to failed user entry until new entry
We left parser scope pointing to one we failed to parse current user
entry and restore previous, or delete if adding new, user data.

If code calling run_usrxml_parser() is designed to skip failed entries
(not currently applicable to load_usrxml_file() helper), we continue on
failed entry returning failure again and trying to restore/delete
user data.

Instead introduce new parser state: error that intended to skip all
lines until next valid user entry begins. This allows to implement code
that skips failed entries.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -309,12 +309,14 @@ function declare_usrxml_consts()
 	USRXML_E_HANDLE_FULL    = -202;
 	USRXML_E_API_ORDER      = -203;
 	USRXML_E_GETLINE        = -204;
+	USRXML_E_EATLINE        = -205;
 	# entry
 	USRXML_E_NOENT	= -301;
 
 	## Constants (internal, arrays get cleaned )
 
 	# Tag scope
+	USRXML__scope_error	= -1;
 	USRXML__scope_none	= 0;
 	USRXML__scope_user	= 1;
 	USRXML__scope_pipe	= 2;
@@ -322,6 +324,7 @@ function declare_usrxml_consts()
 	USRXML__scope_net	= 4;
 	USRXML__scope_net6	= 5;
 
+	USRXML__scope2name[USRXML__scope_error]	= "error";
 	USRXML__scope2name[USRXML__scope_none]	= "none";
 	USRXML__scope2name[USRXML__scope_user]	= "user";
 	USRXML__scope2name[USRXML__scope_pipe]	= "pipe";
@@ -1162,6 +1165,20 @@ function fini_usrxml_parser(h,    n, u)
 # Parse and validate XML document.
 #
 
+function usrxml__scope_error(h, name, val)
+{
+	# Skip all lines until new user entry on error
+
+	if (name == "user") {
+		USRXML__instance[h,"scope"] = USRXML__scope_none;
+
+		# Signal caller to lookup with new scope
+		return 1;
+	}
+
+	return USRXML_E_EATLINE;
+}
+
 function usrxml__scope_none(h, name, val,    n, i)
 {
 	if (name == "user") {
@@ -1555,6 +1572,13 @@ function run_usrxml_parser(h, line, cb, data,    a, nfields, fn, val)
 				val = @cb(h, val, data);
 			else
 				val++;
+			break;
+		}
+		# Parse error in the middle of operation: skip lines until valid
+		if (val < 0) {
+			if (val == USRXML_E_EATLINE)
+				return USRXML_E_NONE;
+			USRXML__instance[h,"scope"] = USRXML__scope_error;
 			break;
 		}
 	} while (val > 0);

commit ea808e9691c3492cfb513f4d33c4517621543599

libusrxml: Clear errno really skip lines of failed entry on next run
Otherwise run_usrxml_parser() will return value of errno without
actually skipping lines.

Fixes: 57444276950b ("libusrxml: Skip lines belonging to failed user entry until new entry")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -2002,10 +2002,6 @@ function usrxml__scope_net6(h, sign, name, val)
 
 function run_usrxml_parser(h, line, cb, data,    a, n, fn, sign, name, val, ret, s_rs, s_rl)
 {
-	val = usrxml_errno(h);
-	if (val != USRXML_E_NONE)
-		return val;
-
 	val = USRXML__instance[h,"order"];
 	if (val < USRXML__order_parse)
 		return usrxml__seterrno(h, USRXML_E_API_ORDER);
@@ -2020,6 +2016,8 @@ function run_usrxml_parser(h, line, cb, data,    a, n, fn, sign, name, val, ret,
 		USRXML__instance[h,"linenum"] = 0;
 	}
 	USRXML__instance[h,"linenum"]++;
+
+	usrxml__clearerrno(h);
 
 	if (line ~ /^[[:space:]]*$/)
 		return USRXML_E_NONE;