serhepopovych/libusrxml

commit d855a1eadfa8d1e52a20ef666333d50e7777a356

libusrxml: Add flag to skip failed user entries in load_usrxml_file()
With this flag we can skip failed entries and continue data load even
from different file. Special scope, called "error", introduced for this
purpose is preserved accross multiple load_usrxml_file() calls.

While there introduce and use helpers to clear errno library variable.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -73,6 +73,20 @@ function usrxml_seterrno(h, val)
 	return usrxml__seterrno(h, val);
 }
 
+function usrxml__clearerrno(h,    val)
+{
+	val = USRXML__instance[h,"errno"];
+	USRXML__instance[h,"errno"] = USRXML_E_NONE;
+	return val;
+}
+
+function usrxml_clearerrno(h)
+{
+	if (!is_valid_usrxml_handle(h))
+		return USRXML_E_HANDLE_INVALID;
+	return usrxml__clearerrno(h);
+}
+
 #
 # Control library verbosity levels.
 #
@@ -345,6 +359,9 @@ function declare_usrxml_consts()
 	# Library public functions call order
 	USRXML__order_none	= 0;
 	USRXML__order_parse	= 1
+
+	# Load/store flags
+	USRXML_LOAD_SKIP_FAILED	= lshift(1, 0);
 
 	# Zone and direction names to mask mapping
 	zone_dir_bits["world","in"]	= 0x01;
@@ -1745,7 +1762,7 @@ function print_usrxml_entries(h, file)
 # Load/store batch from file/pipe/stdin
 #
 
-function load_usrxml_file(_h, file, cb, data,    h, line, rc, ret, s_fn, stdin)
+function load_usrxml_file(_h, file, flags, cb, data,    h, line, rc, ret, s_fn, stdin)
 {
 	stdin = "/dev/stdin";
 
@@ -1769,8 +1786,11 @@ function load_usrxml_file(_h, file, cb, data,    h, line, rc, ret, s_fn, stdin)
 
 	while ((rc = (getline line <FILENAME)) > 0) {
 		ret = run_usrxml_parser(h, line, cb, data);
-		if (ret < 0)
-			break;
+		if (ret < 0) {
+			if (!and(flags, USRXML_LOAD_SKIP_FAILED))
+				break;
+			usrxml__clearerrno(h);
+		}
 	}
 
 	if (file != stdin)
@@ -1781,7 +1801,7 @@ function load_usrxml_file(_h, file, cb, data,    h, line, rc, ret, s_fn, stdin)
 	} else if (ret == USRXML_E_NONE) {
 		# Check for open sections
 		rc = USRXML__instance[h,"scope"];
-		if (rc != USRXML__scope_none)
+		if (rc > USRXML__scope_none)
 			ret = usrxml_scope_err(h, USRXML__scope2name[rc]);
 	}
 

commit d0c5ba9745840e401936858527bc5faaeb1e3fe6

libusrxml: Do not return failure when USRXML_LOAD_SKIP_FAILED used
This flag instructs load_usrxml_file() to skip failures and continue
loading data, returning failure only on I/O errors.

However if callback, passed to run_usrxml_parser() failed we clear errno
but keep returning failure as load_usrxml_file() result, confusing the
caller that passes USRXML_LOAD_SKIP_FAILED.

Fixes: d855a1eadfa8 ("libusrxml: Add flag to skip failed user entries in load_usrxml_file()")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -3921,10 +3921,10 @@ function load_usrxml_file(_h, file, flags, cb, data,    h, line, rc, ret, s_fn, 
 	while ((rc = (getline line <FILENAME)) > 0) {
 		ret = run_usrxml_parser(h, line, cb, data);
 		if (ret < 0) {
-			if (and(flags, USRXML_LOAD_SKIP_FAILED))
-				usrxml__clearerrno(h);
-			else
+			if (!and(flags, USRXML_LOAD_SKIP_FAILED))
 				break;
+			usrxml__clearerrno(h);
+			ret = USRXML_E_NONE;
 		}
 	}
 