serhepopovych/libusrxml

commit 1d032a8db46072b10c3381b496ad051b392fd389

libusrxml: Pass original @data from run_usrxml_parser() to ifupdown func
We might need to pass additional data to @fn called from ifupdown
callback processing USRXML_ifupdown list.

There are two formats of data array:

  1) Hierarcical where @fn stored under "usrxml","ifupdown"
  2) Plain where @fn stored directly in @data array.

Both formats supported with preference to first.

Note that it is not strictly necessary to known in @fn if we perform
normal up/down or rollback: thus we not copying "rb" from a[] to data[].

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -3507,7 +3507,7 @@ function usrxml__scope_net6(h, sign, name, val)
 	return usrxml__scope_nets(h, sign, name, val, USRXML_usernets6, "6");
 }
 
-function usrxml__ifupdown_cb(h, ifname, iflu, a, arr, dec,    ud, fn)
+function usrxml__ifupdown_cb(h, ifname, iflu, data, arr, dec,    ud, fn)
 {
 	# ifdown(-1),ifup(1)
 	ud = arr[h,ifname,iflu];
@@ -3533,14 +3533,25 @@ function usrxml__ifupdown_cb(h, ifname, iflu, a, arr, dec,    ud, fn)
 		}
 	}
 
-	fn = a["fn"];
+	if (isarray(data)) {
+		fn = "usrxml" SUBSEP "ifupdown" SUBSEP "fn";
+		if (fn in data)
+			fn = data[fn];
+		else if ("fn" in data)
+			fn = data["fn"];
+		else
+			fn = "";
+	} else {
+		fn = data;
+	}
+
 	if (fn != "")
-		return @fn(h, ifname, iflu, a, arr, dec);
+		return @fn(h, ifname, iflu, data, arr, dec);
 
 	return 0;
 }
 
-function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
+function usrxml__ifupdown(h, a, data,    b, n, i, p, f, ret, cb, ifname)
 {
 	# h,"num"
 	n = h SUBSEP "num";
@@ -3551,8 +3562,6 @@ function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
 	n = USRXML_ifupdown[n];
 
 	if (!("rb" in a)) {
-		a["fn"] = fn;
-
 		delete a["fwd","p"];
 		delete a["rev","p"];
 	}
@@ -3579,7 +3588,7 @@ function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
 
 		ifname = USRXML_ifupdown[i];
 
-		ret = usrxml___dyn_for_each_reverse_from(h, ifname, cb, a,
+		ret = usrxml___dyn_for_each_reverse_from(h, ifname, cb, data,
 							 f, USRXML_ifupdown);
 		if (split(ret, b, SUBSEP) != 2)
 			continue;
@@ -3591,7 +3600,7 @@ function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
 		a["fwd","f"] = b[2];
 
 		a["rb"] = 1;
-		usrxml__ifupdown(h, a, fn);
+		usrxml__ifupdown(h, a, data);
 
 		return b[1];
 	}
@@ -3614,7 +3623,7 @@ function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
 
 		ifname = USRXML_ifupdown[i];
 
-		ret = usrxml___dyn_for_each_from(h, ifname, cb, a,
+		ret = usrxml___dyn_for_each_from(h, ifname, cb, data,
 						 f, USRXML_ifupdown);
 		if (split(ret, b, SUBSEP) != 2)
 			continue;
@@ -3626,7 +3635,7 @@ function usrxml__ifupdown(h, a, fn,    b, n, i, p, f, ret, cb, ifname)
 		a["rev","f"] = b[2];
 
 		a["rb"] = 1;
-		usrxml__ifupdown(h, a, fn);
+		usrxml__ifupdown(h, a, data);
 
 		return b[1];
 	}

commit 488e91efa93f60dbef35f4078181457cb24af493

libusrxml: Fix ifupdown callback in case of rollback
That callback determines if running rollback by checking "rb" presence
in array @a which is not passed anymore since we pass original @data
from run_usrxml_parser() to ifupdown callback.

Make @a part of @data array supplied to run_usrxml_parser() to pass
both user supplied @data and @a to the ifupdown callback and user @fn.

Also respect that @cb and/or @data of run_usrxml_parser() are optional
thus use local, empty @b array when @data not specified, prepare "a"
subarray in it and do that only when @cb is given.

Fixes: 1d032a8db460 ("libusrxml: Pass original @data from run_usrxml_parser() to ifupdown func")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -3518,8 +3518,10 @@ function usrxml__ifupdown_cb(h, ifname, iflu, data, arr, dec,    ud, fn)
 	# ifdown(-1),ifup(1)
 	ud = arr[h,ifname,iflu];
 
+	r = "usrxml" SUBSEP "ifupdown";
+
 	# Skip entries not matching current iterator
-	if ((dec - 2 * dec * ("rb" in a)) != ud)
+	if ((dec - 2 * dec * usrxml_in_array("rb", data[r,"a"])) != ud)
 		return 0;
 
 	if (ud < 0) {
@@ -3539,17 +3541,10 @@ function usrxml__ifupdown_cb(h, ifname, iflu, data, arr, dec,    ud, fn)
 		}
 	}
 
-	if (isarray(data)) {
-		fn = "usrxml" SUBSEP "ifupdown" SUBSEP "fn";
-		if (fn in data)
-			fn = data[fn];
-		else if ("fn" in data)
-			fn = data["fn"];
-		else
-			fn = "";
-	} else {
-		fn = data;
-	}
+	# "usrxml","ifupdown","fn"
+	fn = r SUBSEP "fn";
+
+	fn = (fn in data) ? data[fn] : "";
 
 	if (fn != "")
 		return @fn(h, ifname, iflu, data, arr, dec);
@@ -3557,8 +3552,23 @@ function usrxml__ifupdown_cb(h, ifname, iflu, data, arr, dec,    ud, fn)
 	return 0;
 }
 
-function usrxml__ifupdown(h, a, data,    b, n, i, p, f, ret, cb, ifname)
+function usrxml__ifupdown(h, data, a,    b, n, i, p, f, ret, cb, ifname)
 {
+	if (!isarray(a)) {
+		n = "usrxml" SUBSEP "ifupdown" SUBSEP "a";
+
+		if (n in data) {
+			if (!isarray(data[n]))
+				return USRXML_E_NOT_ARRAY;
+		} else {
+			# Last resort: create empty array
+			data[n][1] = 1;
+			delete data[n][1];
+		}
+
+		return usrxml__ifupdown(h, data, data[n]);
+	}
+
 	# h,"num"
 	n = h SUBSEP "num";
 
@@ -3606,7 +3616,7 @@ function usrxml__ifupdown(h, a, data,    b, n, i, p, f, ret, cb, ifname)
 		a["fwd","f"] = b[2];
 
 		a["rb"] = 1;
-		usrxml__ifupdown(h, a, data);
+		usrxml__ifupdown(h, data, a);
 
 		return b[1];
 	}
@@ -3641,7 +3651,7 @@ function usrxml__ifupdown(h, a, data,    b, n, i, p, f, ret, cb, ifname)
 		a["rev","f"] = b[2];
 
 		a["rb"] = 1;
-		usrxml__ifupdown(h, a, data);
+		usrxml__ifupdown(h, data, a);
 
 		return b[1];
 	}
@@ -3650,7 +3660,7 @@ function usrxml__ifupdown(h, a, data,    b, n, i, p, f, ret, cb, ifname)
 }
 
 function run_usrxml_parser(h, line, cb, data,
-			   n, r, a, fn, sign, name, val, ret)
+			   n, r, a, b, fn, sign, name, val, ret)
 {
 	# h,"order"
 	n = h SUBSEP "order";
@@ -3731,7 +3741,28 @@ function run_usrxml_parser(h, line, cb, data,
 			a["i"] = ret;
 
 			if (cb != "") {
-				ret = @cb(h, a, data);
+				r = "usrxml" SUBSEP "ifupdown" SUBSEP "a";
+
+				if (isarray(data)) {
+					delete data[r];
+
+					data[r]["id"] = a["id"];
+					data[r]["i"] = a["i"];
+					if (USRXML_orig in a)
+						data[r][USRXML_orig] = a[USRXML_orig];
+
+					ret = @cb(h, data, data[r]);
+				} else {
+					delete b;
+
+					b[r]["id"] = a["id"];
+					b[r]["i"] = a["i"];
+					if (USRXML_orig in a)
+						b[r][USRXML_orig] = a[USRXML_orig];
+
+					ret = @cb(h, b, b[r]);
+				}
+
 				if (ret < 0) {
 					usrxml__restore_if(h, 1);
 					return ret;