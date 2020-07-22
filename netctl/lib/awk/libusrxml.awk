serhepopovych/libusrxml

commit 19fb83cf999e97ee2033502756536319790e22eb

libusrxml: Fix lower "act"ive counter copy
We do not copy such counter while copying lower references dynamic map
and this brokes counting of active interfaces on restore.

Add usrxml__act_copy() helper to accomplish this and make
usrxml__map_copy_one() to follow other copy helpers behaviour by
deleting destination before storing value from source.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -778,6 +778,8 @@ function usrxml__map_copy_one(dh, dmap, sh, smap, attr,    n, i)
 	if (dh == sh)
 		return "";
 
+	usrxml__map_del_by_attr(dh, attr, dmap);
+
 	if (attr ~ "^[[:digit:]]+$") {
 		# sh,id
 		i = sh SUBSEP attr;
@@ -1219,6 +1221,24 @@ function usrxml__type_cmp(h, name,    n, type, cmp, len, num, dyn)
 # Helpers to manage number of "act"ive lowers/uppers
 #
 
+function usrxml___act_copy(dh, sh, dyn, arr)
+{
+	# dh,dyn
+	dh = dh SUBSEP dyn;
+	# sh,dyn
+	sh = sh SUBSEP dyn;
+
+	return usrxml__map_copy_one(dh, arr, sh, arr, "act");
+}
+
+function usrxml__act_copy(dh, sh, dyn, arr)
+{
+	if (isarray(arr))
+		return usrxml___act_copy(dh, sh, dyn, arr);
+	else
+		return usrxml___act_copy(dh, sh, dyn, USRXML__dynmap);
+}
+
 function usrxml____act_adjust(h, dyn, val, arr, dec,    i)
 {
 	# h,dyn,"act"
@@ -1528,6 +1548,8 @@ function usrxml__copy_user(dh, sh, username,
 	i = "lower-" username ":";
 	usrxml__dyn_del_all(dh, i);
 	usrxml__dyn_for_each(sh, i, cb, data);
+
+	usrxml__act_copy(dh, sh, i);
 
 	# net
 	usrxml__copy_user_net(i_dst, i_src, USRXML_usernets);
@@ -1839,6 +1861,8 @@ function usrxml__copy_if(dh, sh, ifname,    i, cb, i_dst, i_src, name, data)
 	i = "lower-" ifname;
 	usrxml__dyn_del_all(dh, i);
 	usrxml__dyn_for_each(sh, i, cb, data);
+
+	usrxml__act_copy(dh, sh, i);
 
 	i = "upper-" ifname;
 	usrxml__dyn_del_all(dh, i);

commit 58f25074c4f346a87f5209a9836975e6dfc38fdb

libusrxml: Do not use usrxml__map_copy_one() when copying "act" counter
Since "act"ive interfaces counter has no relation to map we cannot
use map routines to handle (e.g. copy) it. Especially mentioned
routine tests for "id" field presence that is always false because
number of "act"ive interfaces tracked directly in usrxml__resolve_refs()
and other places. Copy it directly and transit from function to
procedure by omitting result.

Fixes: 19fb83cf999e ("libusrxml: Fix lower "act"ive counter copy")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -1387,15 +1387,18 @@ function usrxml___act_copy(dh, sh, dyn, arr)
 	# sh,dyn
 	sh = sh SUBSEP dyn;
 
-	return usrxml__map_copy_one(dh, arr, sh, arr, "act");
+	if ((sh,"act") in arr)
+		arr[dh,"act"] = arr[sh,"act"];
+	else
+		delete arr[dh,"act"];
 }
 
 function usrxml__act_copy(dh, sh, dyn, arr)
 {
 	if (isarray(arr))
-		return usrxml___act_copy(dh, sh, dyn, arr);
+		usrxml___act_copy(dh, sh, dyn, arr);
 	else
-		return usrxml___act_copy(dh, sh, dyn, USRXML__dynmap);
+		usrxml___act_copy(dh, sh, dyn, USRXML__dynmap);
 }
 
 function usrxml____act_adjust(h, dyn, val, arr, dec,    i)