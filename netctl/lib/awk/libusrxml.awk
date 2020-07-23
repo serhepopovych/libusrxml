serhepopovych/libusrxml

commit 5f75a22c02c3bee115f3295344cb954b5b3a8a7b

libusrxml: Store list of active userids in dynamic map associated value
Some legacy code in retc project still relies on this behaviour and
changing it to use dynamic map would make it hard to maintain due
to lot of variables and code dependencies that cannot be easily dropped.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -1523,7 +1523,7 @@ function usrxml__map_del_umap_attr4map(h, userid, map, umap,    m, i, j, p, val)
 	}
 }
 
-function usrxml__activate_user(h, username,    userid, n, ret)
+function usrxml__activate_user(h, username,    userid, n, ret, dval)
 {
 	# h,username
 	n = h SUBSEP username;
@@ -1532,7 +1532,11 @@ function usrxml__activate_user(h, username,    userid, n, ret)
 
 	# if
 	n = USRXML_userif[h,userid];
-	usrxml___dyn_add_val(h, n, username, userid, USRXML_ifuser);
+
+	dval = usrxml__dyn_get_dval(h, n);
+	dval = (dval != "") ? dval " " userid : userid;
+
+	usrxml___dyn_add_val(h, n, username, userid, USRXML_ifuser, dval);
 
 	# net
 	ret = usrxml__map_add_umap_attr2map(h, userid, USRXML_nets,
@@ -1569,7 +1573,7 @@ function usrxml__activate_user_by_name(h, username,    ret)
 	return USRXML_E_NONE;
 }
 
-function usrxml__deactivate_user_by_name(h, username,    userid, n, ret)
+function usrxml__deactivate_user_by_name(h, username,    userid, n, ret, dval)
 {
 	# h,username
 	n = h SUBSEP username;
@@ -1578,7 +1582,12 @@ function usrxml__deactivate_user_by_name(h, username,    userid, n, ret)
 
 	# if
 	n = USRXML_userif[h,userid];
-	usrxml___dyn_del_by_attr(h, n, username, USRXML_ifuser);
+
+	dval = usrxml__dyn_get_dval(h, n);
+	dval = gensub("^" userid " |( )" userid " | " userid "$", "\\1", "g", dval);
+
+	usrxml___dyn_del_by_attr(h, n, username, USRXML_ifuser, dval);
+
 	# net
 	usrxml__map_del_umap_attr4map(h, userid, USRXML_nets, USRXML_usernets);
 	# net6
@@ -2475,7 +2484,7 @@ function init_usrxml_parser(prog,    h)
 	# ifid = [0 .. nifn - 1]
 	# ifid = USRXML_ifuser[h,userif,"id"]
 	# userif = USRXML_ifuser[h,ifid]
-	# nifu = USRXML_ifuser[h,userif]
+	# userids = USRXML_ifuser[h,userif]
 	#
 	# nifu = USRXML_ifuser[h,userif,"num"]
 	# iuid = [0 .. nifu - 1]

commit 8ce9e1b23181af25711003c659018cbd28a86c6f

libusrxml: Give USRXML_ifuser[] to usrxml__dyn_get_dval() explicitly
Otherwise usrxml__dyn_get_dval() will use USRXML__dynmap[] and always
return empty value for userids list that assumed to be present for
compatibility with dynamic array unaware code.

Fixes: 5f75a22c02c3 ("libusrxml: Store list of active userids in dynamic map associated value")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -1536,7 +1536,7 @@ function usrxml__activate_user(h, username,    userid, n, ret, dval)
 	# if
 	n = USRXML_userif[h,userid];
 
-	dval = usrxml__dyn_get_dval(h, n);
+	dval = usrxml__dyn_get_dval(h, n, USRXML_ifuser);
 	dval = (dval != "") ? dval " " userid : userid;
 
 	usrxml___dyn_add_val(h, n, username, userid, USRXML_ifuser, dval);
@@ -1586,7 +1586,7 @@ function usrxml__deactivate_user_by_name(h, username,    userid, n, ret, dval)
 	# if
 	n = USRXML_userif[h,userid];
 
-	dval = usrxml__dyn_get_dval(h, n);
+	dval = usrxml__dyn_get_dval(h, n, USRXML_ifuser);
 	dval = gensub("^" userid " |( )" userid " | " userid "$", "\\1", "g", dval);
 
 	usrxml___dyn_del_by_attr(h, n, username, USRXML_ifuser, dval);