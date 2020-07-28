serhepopovych/libusrxml

commit 888526fca204503fda62943da97de00bf7a5be95

libusrxml: Make usrxml__map_copy() to cleanup destination before copy
Use usrxml__map_del_all() to cleanup destination before copy. Add attrs
using usrxml__map_add_val() instead direct, one-to-one, assignment to
consolidate possible holes on copy.

Return number of elements copied from source to destination map.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -1071,30 +1071,36 @@ function usrxml__map_del_all(h, map,    n, i, p, cnt)
 	return cnt;
 }
 
-function usrxml__map_copy(dh, dmap, sh, smap,    n, p, attr)
+function usrxml__map_copy(dh, dmap, sh, smap,    n, i, p, attr)
 {
-	# Not touching if source is empty
-	if (!((sh,"num") in smap))
-		return;
+	# sh,"num"
+	i = sh SUBSEP "num";
 
-	n = smap[sh,"num"];
-	if (!n)
-		return;
+	if (!(i in smap))
+		return 0;
 
-	dmap[dh,"num"] = n;
-	dmap[dh,"min"] = smap[sh,"min"];
-	dmap[dh,"max"] = smap[sh,"max"];
-	dmap[dh,"cnt"] = smap[sh,"cnt"];
+	if (dh == sh)
+		return 0;
 
+	usrxml__map_del_all(dh, dmap);
+
+	n = smap[i];
 	for (p = 0; p < n; p++) {
+		# sh,id
+		i = sh SUBSEP p;
+
 		# Skip holes entries
-		if (!((sh,p) in smap))
+		if (!(i in smap))
 			continue;
 
-		attr = dmap[dh,p] = smap[sh,p];
-		dmap[dh,attr,"id"] = p;         # optimization
-		dmap[dh,attr] = smap[sh,attr];
+		attr = smap[i];
+		usrxml__map_add_val(dh, attr, dmap, smap[sh,attr]);
 	}
+
+	# dh,"cnt"
+	i = dh SUBSEP "cnt";
+
+	return (i in dmap) ? dmap[i] : 0;
 }
 
 #

commit 5c236c6e4e84fda09fabd3d551e0da7019edc14d

libusrxml: Make exact copy of source map in destination
Previously source and destination maps aren't exactly same since copy
is done with help of usrxml__map_add_val() to consolidate holes in
element numbering if source map has gaps after elements removal.

Now it was discovered that usrxml__copy_user_net() introduces leak on
if/user entry save/restore before/after modification because
usrxml__map_copy_all() may use element index that differs from one used
to copy additional data associated with <net> and/or <net6> tags.

Following config snippet could be used to reproduce issue:

  <host en3>
  </host>

  <user WN2020072201>
    <if en3>
    <net 192.0.2.1>
      <src 192.0.2.254>
    </net>
    <net 192.0.2.2>
      <src 195.234.71.254>
    </net>
  </user>

  <user WN2020072201>
    <-net 195.234.71.1>
  </user>

  <user WN2020072201>
  </user>

Fix by partially reverting commit 888526fca204 ("libusrxml: Make
usrxml__map_copy() to cleanup destination before copy") to restore
original behaviour where elements copied one by one making exact copy
of source in destination.

Fixes: 888526fca204 ("libusrxml: Make usrxml__map_copy() to cleanup destination before copy")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -774,20 +774,28 @@ function usrxml__map_del_all(h, map,    n, i, p, cnt)
 	return cnt;
 }
 
-function usrxml__map_copy_all(dh, dmap, sh, smap,    n, i, p, attr)
+function usrxml__map_copy_all(dh, dmap, sh, smap,    n, i, p, attr, cnt)
 {
+	if (dh == sh)
+		return 0;
+
 	# sh,"num"
 	i = sh SUBSEP "num";
 
 	if (!(i in smap))
 		return 0;
 
-	if (dh == sh)
+	n = smap[i];
+	if (!n)
 		return 0;
 
 	usrxml__map_del_all(dh, dmap);
 
-	n = smap[i];
+	dmap[dh,"num"] = n;
+	dmap[dh,"min"] = smap[sh,"min"];
+	dmap[dh,"max"] = smap[sh,"max"];
+	dmap[dh,"cnt"] = cnt = smap[sh,"cnt"];
+
 	for (p = 0; p < n; p++) {
 		# sh,id
 		i = sh SUBSEP p;
@@ -797,13 +805,13 @@ function usrxml__map_copy_all(dh, dmap, sh, smap,    n, i, p, attr)
 			continue;
 
 		attr = smap[i];
-		usrxml__map_add_val(dh, attr, dmap, smap[sh,attr]);
+
+		dmap[dh,p] = attr;
+		dmap[dh,attr] = smap[sh,attr];
+		dmap[dh,attr,"id"] = p;
 	}
 
-	# dh,"cnt"
-	i = dh SUBSEP "cnt";
-
-	return (i in dmap) ? dmap[i] : 0;
+	return cnt;
 }
 
 function usrxml__map_copy_one(dh, dmap, sh, smap, attr,    n, i)