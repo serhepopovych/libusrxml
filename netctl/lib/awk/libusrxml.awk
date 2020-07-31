serhepopovych/libusrxml

commit 8b62cebb636b8042ae299bbd397a43d74c167dba

libusrxml: Extend interface/user copy helpers
Make copied data consistent by correcting unresolved entries when
necessary (e.g. unres-en0.32,en0 = "upper" exists but
upper-en0,en0.32 != "?" or upper-en0,en0.32 is set to "?" but no
unres-en0.32,en0 exists).

Never copy unresolved references for deleted entries: this will prevent
from unnecessary resolution of deleted (i.e. whose value set to "/")
lowers/uppers in copied entry that pointing to non-existend (unresolved)
item.

Add parameter @new to usrxml__copy_if_by_name() to skip copying of
unresolved entries, if any, and forcing value to always be empty
(new entry, see usrxml__resolve_refs()) or marked for delete (i.e. "/").

This could be used to copy item making it appear as newly added
for use with usrxml__resolve_refs().

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -1827,26 +1827,46 @@ function usrxml__deactivate_if_by_name(h, dyn, iflu, ifname, arr,    i, cb, val)
 	return int(usrxml__dyn_for_each(h, "upper-" iflu, cb, iflu));
 }
 
-function usrxml__copy_if_cb(sh, dyn, iflu, data, arr, dec,    val, dh, ifname)
+function usrxml__copy_if_cb(sh, dyn, iflu, data, arr, dec,    t, val, dh, ifname)
 {
 	dh = data["dh"];
 	ifname = data["ifname"];
 
-	usrxml___dyn_add_val(dh, dyn, iflu, arr[sh,dyn,iflu], arr);
+	# sh,unres-{iflu}
+	res = "unres-" iflu;
 
-	dyn = "unres-" iflu;
+	val = arr[sh,dyn,iflu];
 
-	# sh,unres-{iflu},ifname
-	val = usrxml__dyn_get_val(sh, dyn, ifname, arr);
-	if (val != "")
-		usrxml___dyn_add_val(dh, dyn, ifname, val, arr);
+	if (data["new"]) {
+		if (val != "/")
+			val = "";
+		t = "";
+	} else {
+		t = usrxml__dyn_get_val(sh, res, ifname, arr);
+
+		if (t != "") {
+			if (val != "/")
+				val = "?";
+			else
+				t = "";
+		} else {
+			if (val == "?")
+				t = data["lu"];
+		}
+	}
+
+	if (t != "")
+		usrxml___dyn_add_val(dh, res, ifname, t, arr);
 	else
-		usrxml___dyn_del_by_attr(dh, dyn, ifname, arr);
+		usrxml___dyn_del_by_attr(dh, res, ifname, arr);
+
+	usrxml___dyn_add_val(dh, dyn, iflu, val, arr);
 
 	return 0;
 }
 
-function usrxml__copy_if_by_name(dh, sh, ifname,    i, i_dst, i_src, name, cb, data)
+function usrxml__copy_if_by_name(dh, sh, ifname, new,
+				 i, i_dst, i_src, name, cb, data)
 {
 	# sh,ifname
 	i_src = sh SUBSEP ifname;
@@ -1866,6 +1886,7 @@ function usrxml__copy_if_by_name(dh, sh, ifname,    i, i_dst, i_src, name, cb, d
 
 	data["dh"] = dh;
 	data["ifname"] = ifname;
+	data["new"] = new;
 
 	if (usrxml__type_is_user(sh, ifname, USRXML_ifnames[i_src]))
 		return usrxml__copy_user(dh, i_dst, sh, i_src, ifname, cb, data);
@@ -1883,13 +1904,15 @@ function usrxml__copy_if_by_name(dh, sh, ifname,    i, i_dst, i_src, name, cb, d
 	i_dst = usrxml__map_add_val(dh, ifname,
 				    USRXML_ifnames, USRXML_ifnames[i_src]);
 
-	i = "lower-" ifname;
+	data["lu"] = i = "lower";
+	i = i "-" ifname;
 	usrxml__dyn_del_all(dh, i);
 	usrxml__dyn_for_each(sh, i, cb, data);
 
 	usrxml__act_copy(dh, sh, i);
 
-	i = "upper-" ifname;
+	data["lu"] = i = "upper";
+	i = i "-" ifname;
 	usrxml__dyn_del_all(dh, i);
 	usrxml__dyn_for_each(sh, i, cb, data);
 

commit 9d73f10e461c280cdce25c71696714f3168af014

libusrxml: Add res local variable to usrxml__copy_if_cb()
Fixes: commit 8b62cebb636b ("libusrxml: Extend interface/user copy helpers")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/awk/libusrxml.awk
@@ -2090,7 +2090,8 @@ function usrxml__deactivate_if_by_name(h, dyn, iflu, ifname, arr,    i, cb, val)
 	return int(usrxml__dyn_for_each(h, "upper-" iflu, cb, iflu));
 }
 
-function usrxml__copy_if_cb(sh, dyn, iflu, data, arr, dec,    t, val, dh, ifname)
+function usrxml__copy_if_cb(sh, dyn, iflu, data, arr, dec,
+			    t, res, val, dh, ifname)
 {
 	dh = data["dh"];
 	ifname = data["ifname"];